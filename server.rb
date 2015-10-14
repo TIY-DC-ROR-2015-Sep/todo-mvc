db3a3a418c9269c627c65f0c7e6549271965f1f7
James Dabbs <jamesdabbs@gmail.com>
2015-10-14 11:03:58 -0400

    Add JSON API section

--- server.rb
+++ server.rb
@@ -11,9 +11,36 @@ class App < Sinatra::Base
     "It works!"
   end
 
+  # HTML section
+
   get "/lists/:list_id" do
     list = List.find params[:list_id]
     erb :list, locals: { list: list }
+  end
+
+  post "/lists/:list_id/todos" do
+    Item.create(
+      name:    params[:item_name],
+      list_id: params[:list_id]
+    )
+    redirect to("/lists/#{params[:list_id]}")
+  end
+
+  # JSON API
+
+  get "/api/lists/:list_id" do
+    list = List.find params[:list_id]
+    { id:    list.id,
+      name:  list.name,
+      items: list.items }.to_json
+  end
+
+  post "/api/lists/:list_id/todos" do
+    i = Item.create(
+      name:    params[:item_name],
+      list_id: params[:list_id]
+    )
+    i.to_json
   end
 end
 
